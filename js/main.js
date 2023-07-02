92da274acf53b6497a799588225351525f71cdd4
Asanali <as.tursynkhanov@gmail.com>
2023-07-02 22:58:41 +0600

    first commit American Corner

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function (){
+
+    $('.nav_rev > div').click(function () {
+        const revID = $(this).data('revid')
+        $('.nav_rev > div').not(this).removeClass('active')
+        $(this).addClass('active')
+        $('.item_rev').not(revid).removeClass('active')
+        $(revID).addClass('active')
+    })   
+
+    $('hamburger').click(function () {
+        $(this).toggleClass('is-active')
+
+        if ($(this).hasClass('is-active')) {
+            $('.mnu_top').slideDown(300)
+        } else {
+            $('.mnu_top').slideUp(300)
+        }
+    })
+})
+
+
